273f88d7fbc7bea3e4d221201976a1db885f5bd4
Andres <serdnah2@serdnah2.(none)>
2013-04-06 23:53:20 -0500

    Jsearch Version 4.3

--- js/databasefolder.js
+++ js/databasefolder.js
@@ -1,1 +1,1 @@
-[{"title":"alert.png","link":"http:\/\/localhost\/jsearch\/files\/imagenes\/alert.png","description":"","claves":""},{"title":"folder hi","link":"http:\/\/localhost\/jsearch\/files\/imagenes\/test_folder\/index.html","description":"hi test","claves":"folder"},{"title":"Jserach","link":"http:\/\/localhost\/jsearch\/files\/test_file.html","description":"jsearch test file","claves":"index"}]
+[]
